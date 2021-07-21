f018a9ab7385bf464fb8b438e10fd8e6983c8b57
Виктор Кузьминский <maksimboginic@Maksims-MacBook-Pro.local>
2021-07-21 21:41:58 +0300

    change config

--- data/config.py
+++ data/config.py
@@ -1,15 +1,17 @@
 from environs import Env
 
 # Теперь используем вместо библиотеки python-dotenv библиотеку environs
-env = Env()
-env.read_env()
-
-BOT_TOKEN = env.str("BOT_TOKEN")# Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-IP = env.str("IP")  # Тоже str, но для айпи адреса хоста
-PG_USER = env.str("PG_USER")
-PG_PASSWORD = env.str("PG_PASSWORD")
-DATABASE = env.str("DATABASE")
-ADMIN_BOT = env.str('ADMIN_BOT')
+BOT_TOKEN = '1849927581:AAE31wEKYZ-3bbsFWjzC35nAIMW-gDZnayI'
+ADMIN_BOT = '1922146668:AAHME-AxbZInHNtDcKjYYVO1PvvFoR8OweI'
+ADMINS = [474053240]
+IP = 'localhost'
+PG_USER = 'postgres'
+PG_PASSWORD = 'vick1715'
+DATABASE = 'botapp'
+MERCHANT_ID = 'e9d0f828c9b8b52f00631e4b6789b0fb'
+SECRET = 'a4533e9799acd0e92a9800c20a10d556'
+QIWI_API_KEY = '4388de24c629234586e618ee92370796'
+QIWI_NUMBER = '+7 999 925‑62‑05'
+ITEM_NAME = 'MoneyTransfer'
 
 POSTGRES_URL = f"postgresql://{PG_USER}:{PG_PASSWORD}@{IP}/{DATABASE}"
