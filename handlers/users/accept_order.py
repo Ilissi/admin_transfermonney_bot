ed137f39ed0cd512704fee2ac40be5cc2fbf24de
Виктор Кузьминский <maksimboginic@Maksims-MacBook-Pro.local>
2021-07-24 03:09:09 +0300

    simple

--- handlers/users/accept_order.py
+++ handlers/users/accept_order.py
@@ -5,6 +5,7 @@ import typing
 
 from utils.callback import order_cb
 from utils.db_api.order_controller import get_orders, update_order
+from utils.db_api.user_controller import get_user_id, update_user_balance_set
 from states.accept_order import Order
 from loader import dp, bot_admin
 
@@ -39,6 +40,9 @@ async def remove_message(message: Message, state: FSMContext):
     await state.finish()
     await update_order(int(ticker_data['ticker_id']), message.text, True)
     ticker = await get_orders(int(ticker_data['ticker_id']))
+    user = await get_user_id(ticker.user_id)
+    new_balance = round(user.balance + ticker.amount_spend, 2)
+    await update_user_balance_set(ticker.user_id, new_balance)
     await bot_admin.send_message(ticker.user_id, 'Пополнение ID {}\n<b>Отклонено</b>'.format(ticker.id))
     await message.answer('Действие подтверждено')
 
